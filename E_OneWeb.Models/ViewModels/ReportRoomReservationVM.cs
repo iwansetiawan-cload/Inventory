65f35205dabc72def7f9cef4bb688c9ebc24db37
iwan setiawan <iwan.s@rds.co.id>
2024-01-09 23:53:24 +0700

    Report Pinjam Ruangan

---
+++ E_OneWeb.Models/ViewModels/ReportRoomReservationVM.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_OneWeb.Models.ViewModels
+{
+    public class ReportRoomReservationVM
+    {
+        public string? SearchRoomName { get; set; }
+        public int? SearchRoomId { get; set; }
+        public DateTime? SearchStartDate { get; set; }
+        public DateTime? SearchEndDate { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> MonthList { get; set; }
+    }
+    public class ExportReportRoomReservation
+    {
+        public int? Number { get; set; }
+        public string? RoomName { get; set; }
+        public string? StartDate { get; set; }
+        public string? EndDate { get; set; }
+        public string? Description { get; set; }
+        public string? EntryBy { get; set; }
+        public string? Study { get; set; }
+        public string? Teacher { get; set; }
+    }
+}
--- E_OneWeb/Areas/Admin/Controllers/ReportRoomReservationController.cs
+++ E_OneWeb/Areas/Admin/Controllers/ReportRoomReservationController.cs
@@ -14,49 +14,114 @@ namespace E_OneWeb.Areas.Admin.Controllers
     public class ReportRoomReservationController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private static ReportRoomReservationVM staticvm = new ReportRoomReservationVM();
         public ReportRoomReservationController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
-        public async Task<IActionResult> Index(ItemsVM vm)
+        public async Task<IActionResult> Index(ReportRoomReservationVM vm)
         {
-            IEnumerable<Category> CatList = await _unitOfWork.Category.GetAllAsync();
-            vm.CategoryList = CatList.Select(i => new SelectListItem
-            {
-                Text = i.Name,
-                Value = i.Id.ToString()
-            });
-            var StatusList = _unitOfWork.Genmaster.GetAll().Where(z => z.GENFLAG == 2).Select(x => new SelectListItem { Value = x.IDGEN.ToString(), Text = x.GENNAME });
-            var OwnershipList = _unitOfWork.Genmaster.GetAll().Where(z => z.GENFLAG == 3).Select(x => new SelectListItem { Value = x.IDGEN.ToString(), Text = x.GENNAME });
-            ViewBag.StatusList = new SelectList(StatusList, "Value", "Text");
-            ViewBag.OwnershipList = new SelectList(OwnershipList, "Value", "Text");
+            DateTime now = DateTime.Now;
+            DateTime dtnow = new DateTime(now.Year, now.Month, 1);
+            vm.SearchStartDate = vm.SearchStartDate == null ? dtnow : vm.SearchStartDate;
+            vm.SearchEndDate = vm.SearchEndDate == null ? DateTime.Now : vm.SearchEndDate;
+            staticvm = vm;
             return View(vm);
-        }
+        }     
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
 
-            var datalist = (from z in await _unitOfWork.Items.GetAllAsync(includeProperties: "Category,Room")
+            var datalist = (from z in await _unitOfWork.RoomReservationUser.GetAllAsync(includeProperties: "RoomReservationAdmin")
                             select new
                             {
                                 id = z.Id,
-                                code = z.Code,
-                                name = z.Name,
+                                room = z.RoomReservationAdmin.RoomName + " (" + z.RoomReservationAdmin.LocationName + ")",
+                                startdate = z.StartDate.Value.ToString("dd/MM/yyyy HH:mm"),
+                                enddate = z.EndDate.Value.ToString("dd/MM/yyyy HH:mm"),
+                                startdate_ = z.StartDate,
+                                enddate_ = z.EndDate,
                                 description = z.Description,
-                                category = z.Category.Name,
-                                room = z.Room.Name,
-                                qty = z.Qty,
-                                price = z.Price != null ? z.Price : 0,
-                                totalamount = z.TotalAmount != null ? z.TotalAmount : 0
+                                entryby = z.EntryBy,
+                                roomid = z.RoomReservationAdmin.RoomId
                             }).ToList();
 
+            if (staticvm.SearchStartDate != null && staticvm.SearchEndDate != null)
+            {
+                datalist = datalist.Where(o => o.startdate_ >= staticvm.SearchStartDate && o.enddate_ <= staticvm.SearchEndDate).ToList();
+            }
+            if (staticvm.SearchRoomId != null)
+            {
+                datalist = datalist.Where(o => o.roomid == staticvm.SearchRoomId).ToList();
+            }
+
             return Json(new { data = datalist });
+        }
+        [HttpGet]
+        public IActionResult GetAllRoomAndLocation()
+        {
+            var datalist = (from z in _unitOfWork.Room.GetAll(includeProperties: "Location")
+                            select new
+                            {
+                                id = z.Id,
+                                name_of_room = z.Name,
+                                description = z.Description,
+                                name_of_location = z.Location.Name,
+                                name_of_room_and_location = z.Name + " (" + z.Location.Name + ")"
+                            }).ToList().OrderByDescending(o => o.id);
+            return Json(new { data = datalist });
+        }
+        public async Task<FileResult> Export()
+        {
+
+
+            var datalist = (from z in await _unitOfWork.RoomReservationUser.GetAllAsync(includeProperties: "RoomReservationAdmin")
+                            select new
+                            {
+                                id = z.Id,
+                                room = z.RoomReservationAdmin.RoomName + " (" + z.RoomReservationAdmin.LocationName + ")",
+                                startdate = z.StartDate.Value.ToString("dd/MM/yyyy HH:mm"),
+                                enddate = z.EndDate.Value.ToString("dd/MM/yyyy HH:mm"),
+                                startdate_ = z.StartDate,
+                                enddate_ = z.EndDate,
+                                description = z.Description,
+                                entryby = z.EntryBy,
+                                study = z.Study,
+                                teacher = z.Dosen,
+                                roomid = z.RoomReservationAdmin.RoomId
+                            }).ToList();
+
+            if (staticvm.SearchStartDate != null && staticvm.SearchEndDate != null)
+            {
+                datalist = datalist.Where(o => o.startdate_ >= staticvm.SearchStartDate && o.enddate_ <= staticvm.SearchEndDate).ToList();
+            }
+            if (staticvm.SearchRoomId != null)
+            {
+                datalist = datalist.Where(o => o.roomid == staticvm.SearchRoomId).ToList();
+            }
+
+            int Number = 1;
+            var datalist_ = (from z in datalist
+                             select new ExportReportRoomReservation
+                             {
+                                 Number = Number++,
+                                 RoomName = z.room,
+                                 StartDate = z.startdate,
+                                 EndDate = z.enddate,
+                                 Description = z.description,
+                                 EntryBy = z.entryby,
+                                 Study = z.study,
+                                 Teacher = z.teacher
+                             }).ToList();
+            var file = CreateFile(datalist_);
+            return File(file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Data_Peminjaman_Ruangan.xlsx");
+
         }
         public static byte[] CreateFile<T>(List<T> source)
         {
             var workbook = new XSSFWorkbook();
-            var sheet = workbook.CreateSheet("Sheet1");
-            var rowHeader = sheet.CreateRow(0);
+            var sheet = workbook.CreateSheet("Sheet1");     
+            var rowHeader = sheet.CreateRow(2);          
 
             var properties = typeof(T).GetProperties();
 
@@ -66,37 +131,47 @@ namespace E_OneWeb.Areas.Admin.Controllers
             var style = workbook.CreateCellStyle();
             style.SetFont(font);
 
+            var rowHeaderName = sheet.CreateRow(0);
+            var cell_ = rowHeaderName.CreateCell(0);
+            cell_.SetCellValue("DATA PEMINJAMAN RUANGAN");
+            cell_.CellStyle = style;
+
             var cell = rowHeader.CreateCell(0);
-            cell.SetCellValue("NO");
+            cell.SetCellValue("No");
             cell.CellStyle = style;
+
             cell = rowHeader.CreateCell(1);
-            cell.SetCellValue("CODE");
+            cell.SetCellValue("Nama Ruangan");
             cell.CellStyle = style;
+
             cell = rowHeader.CreateCell(2);
-            cell.SetCellValue("ITEM NAME");
+            cell.SetCellValue("Tanggal Mulai Pinjam");
             cell.CellStyle = style;
+
             cell = rowHeader.CreateCell(3);
-            cell.SetCellValue("DESCRIPTION");
+            cell.SetCellValue("Tanggal Selesai Pinjam");
             cell.CellStyle = style;
+
             cell = rowHeader.CreateCell(4);
-            cell.SetCellValue("CATEGORY");
+            cell.SetCellValue("Keterangan");
             cell.CellStyle = style;
+
             cell = rowHeader.CreateCell(5);
-            cell.SetCellValue("ROOM NAME");
+            cell.SetCellValue("Peminjam");
             cell.CellStyle = style;
+
             cell = rowHeader.CreateCell(6);
-            cell.SetCellValue("QTY");
+            cell.SetCellValue("Mata Kuliah");
             cell.CellStyle = style;
+
             cell = rowHeader.CreateCell(7);
-            cell.SetCellValue("PRICE");
+            cell.SetCellValue("Dosen");
             cell.CellStyle = style;
-            cell = rowHeader.CreateCell(8);
-            cell.SetCellValue("TOTAL AMOUNT");
-            cell.CellStyle = style;
+
             //end header
 
             //content
-            var rowNum = 1;
+            var rowNum = 3;
             foreach (var item in source)
             {
                 var rowContent = sheet.CreateRow(rowNum);
@@ -144,51 +219,6 @@ namespace E_OneWeb.Areas.Admin.Controllers
             var content = stream.ToArray();
 
             return content;
-        }
-        [HttpGet("Export")]
-        public async Task<FileResult> Export(string code)
-        {
-
-            var products = (from z in await _unitOfWork.Items.GetAllAsync(includeProperties: "Category,Room")
-                            select new
-                            {
-                                number = z.Id,
-                                code = z.Code,
-                                name = z.Name,
-                                description = z.Description,
-                                category = z.Category.Name,
-                                room = z.Room.Name,
-                                qty = z.Qty,
-                                price = z.Price != null ? z.Price : 0,
-                                totalamount = z.TotalAmount != null ? z.TotalAmount : 0
-
-                            }).ToList();
-
-            if (code != null)
-            {
-                products = products.Where(z => z.code == code).ToList();
-            }
-            int Number = 1;
-            products = (from z in products
-                        select new
-                        {
-                            number = Number++,
-                            code = z.code,
-                            name = z.name,
-                            description = z.description,
-                            category = z.category,
-                            room = z.room,
-                            qty = z.qty,
-                            price = z.price != null ? z.price : 0,
-                            totalamount = z.totalamount != null ? z.totalamount : 0
-
-                        }).ToList();
-
-
-            var file = CreateFile(products);
-            return File(file, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Export_data_item.xlsx");
-
-
         }
     }
 }
